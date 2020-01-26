a4552dd17e73dd26a30e26e7bb081f2a43627625
agconti <agc11d@gmail.com>
2020-01-26 12:53:45 -0500

    chore: repro example

---
+++ MyComp.js
@@ -0,0 +1,9 @@
+import styles from './MyComp.module.css'
+
+export default function MyComp() {
+  return (
+    <p className={styles.blue}>
+      MyComp
+    </p>
+  )
+}
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import MyComp from '../MyComp'
+
+const Index = () => (
+  <div>
+    <MyComp />
+    <p>Hello Next.js</p>
+  </div>
+)
+
+export default Index
